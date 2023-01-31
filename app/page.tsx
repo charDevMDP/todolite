8a3b172cd6eff080bb50a2dc49cae51416097290
char.dev <char.developer@gmail.com>
2023-01-31 13:16:41 -0300

    cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className='bg-gray-200'>
-      <p className='text-2xl'>Bienvenido a TODOlite un pequeño crud de tareas</p>
+    <div className='bg-gray-200 py-5'>
+      <p className='text-2xl text-center'>Bienvenido a TODOlite un pequeño crud de tareas</p>
     </div>
   )
 }
